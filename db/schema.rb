2282f349927320cb91e36786125b18c472f6dfc7
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2021-12-26 18:33:53 +0100

    Add houses file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_172216) do
+ActiveRecord::Schema.define(version: 2021_12_26_173314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2021_12_26_172216) do
   create_table "favourites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "house_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "houses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "location"
+    t.integer "price"
+    t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
