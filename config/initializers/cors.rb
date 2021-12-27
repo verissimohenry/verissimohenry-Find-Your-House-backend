48d0448658f91b3d3b1863964d7edb5acf30a714
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2021-12-27 11:49:53 +0100

    modify app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
   
     allow do
-      origins 'https://agile-anchorage-14862.herokuapp.com/'
+      origins 'https://tranquil-shore-53426.herokuapp.com/'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
   
