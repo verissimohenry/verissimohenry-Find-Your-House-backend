b93b82bacd5b4f8cc04612bd66b5c195430aa403
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2021-12-27 11:12:26 +0100

    My first commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,16 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3000'
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  
+    allow do
+      origins 'https://agile-anchorage-14862.herokuapp.com/'
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+  
+    allow do
+      origins 'https://friendly-yonath-3ed2b1.netlify.app/'
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    end
+end
