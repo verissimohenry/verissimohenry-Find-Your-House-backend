8ebc90621ec30b2beb5f604a88373dd4045ecced
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-03 09:25:52 +0100

    modify app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
       origins 'http://localhost:3000'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
     end
   
     allow do
       origins 'https://tranquil-shore-53426.herokuapp.com/'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
     end
   
     allow do
       origins 'https://friendly-yonath-3ed2b1.netlify.app/'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
     end
 end
