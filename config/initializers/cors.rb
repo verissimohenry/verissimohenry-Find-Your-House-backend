4517398b67150a67c577fe463639149a34ec869f
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-04 12:57:44 +0100

    Modify cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,18 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:3000'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-    end
-  
-    allow do
-      origins 'https://tranquil-shore-53426.herokuapp.com/'
-      resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
-    end
-  
-    allow do
-      origins 'https://frontend-deploy.vercel.app/'
+      origins 'http://localhost:3000', 'https://frontend-deploy.vercel.app/'
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
     end
