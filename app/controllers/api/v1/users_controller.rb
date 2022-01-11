dc97d1de3a5707d26e34973fe3fc565c4a0020b2
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-11 15:22:48 +0100

    modify app

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::UsersController < ApplicationController
   skip_before_action :authorize_request, only: %i[create]
+  def new
+    @user = User.new
+  end
 
   def create
     user = User.create!(user_params)
