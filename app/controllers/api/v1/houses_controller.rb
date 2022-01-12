26cd42b737710cc5dd4d0bcf54a6c03c7cf6e25d
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-12 14:16:58 +0100

    modify app

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::HousesController < ApplicationController
   skip_before_action :authorize_request
   def index
     @houses = House.all
-    render json: @houses
+    json_response(@houses)
   end
 
   def create
     @house = House.create!(house_params)
-    render json(@house, :created)
+    json_response(@house, :created)
   end
 
   private
