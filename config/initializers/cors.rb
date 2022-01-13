53fad7b0eb489e8125eb845f8b9e7fc8f8abd2c0
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-13 16:02:15 +0100

    modify app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*", "https://friendly-yonath-3ed2b1.netlify.app/"
-    resource "*", headers: :any, methods: [:get, :post, :patch, :put]
+    origins '*', 'https://friendly-yonath-3ed2b1.netlify.app/'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
