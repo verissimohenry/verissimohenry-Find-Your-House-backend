c8de7bf9837a40cd2353d0d3a3122949b12b8248
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-13 19:16:36 +0100

    modify gemfile

--- db/migrate/20211226172216_create_favourites.rb
+++ db/migrate/20211226172216_create_favourites.rb
@@ -1,4 +1,4 @@
-class CreateFavourites < ActiveRecord::Migration[6.1]
+class CreateFavourites < ActiveRecord::Migration[6.0]
   def change
     create_table :favourites do |t|
       t.integer :user_id
--- db/migrate/20211226173314_create_houses.rb
+++ db/migrate/20211226173314_create_houses.rb
@@ -1,4 +1,4 @@
-class CreateHouses < ActiveRecord::Migration[6.1]
+class CreateHouses < ActiveRecord::Migration[6.0]
   def change
     create_table :houses do |t|
       t.string :name
