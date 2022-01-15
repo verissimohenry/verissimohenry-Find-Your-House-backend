1de4d655823743122e6deeb0ff9fdd1243a3c90f
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-15 15:29:58 +0100

    modify app

--- app/services/json_web_token.rb
+++ app/services/json_web_token.rb
@@ -4,7 +4,7 @@ class JsonWebToken
 
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, SECRET_KEY)
+      JWT.encode(payload, SECRET_KEY, 'HS256')
     end
 
     def decode(token)
