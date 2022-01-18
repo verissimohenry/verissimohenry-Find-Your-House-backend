b9b470b4a051050a710a9e5e920c6e2eb25c4ba0
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-18 09:40:13 +0100

    modify app

--- config/application.rb
+++ config/application.rb
@@ -13,30 +13,22 @@ require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require "rails/test_unit/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-ActiveSupport::Deprecation.silenced = true
-ActiveSupport::Deprecation.behavior = :silence
 Bundler.require(*Rails.groups)
 
-module ProjectFindYourHouse
+module FindYourHouse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-    config.active_support.cache_format_version = 6.0
+    config.api_only = true
     config.debug_exception_response_format = :api
-
+    config.load_defaults 6.0
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
 
     # Load files by specifying the lib folder as one of the folders in the list of RoR _autoload_s
     config.eager_load_paths << Rails.root.join('lib')
