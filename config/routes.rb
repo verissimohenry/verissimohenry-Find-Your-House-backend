f96bffa5b7205983fe4768cb040450d19a3f1271
HENRY VERISSIMO <VERISSIMOHENRY04@GMAIL.COM>
2022-01-18 10:18:47 +0100

    modify app

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  root 'houses#index'
-
-  resources :sessions, only: [:new, :create, :show, :index]
-  resources :registrations, only: [:new, :create, :show, :index]
-  resources :users, only: [:new, :create, :show, :index]
-  resources :houses, param: :dscription, only: [:index, :create]
-  resources :favourites, only: [:create, :destroy]
-  
-  resources :tokens, only: [:create]
-
-      post '/auth/login', to: 'authentications#create'
-      post '/signup', to: 'users#create'
-
-  get '*path', to: 'houses#index', via: :all
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  resources :houses
+  resources :favourites
 end
