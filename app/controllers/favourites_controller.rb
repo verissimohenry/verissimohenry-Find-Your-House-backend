2287a2d2ba7123651c17907ce25cb7dd102d6943
verissimohenry <verissimohenry04@gmail.com>
2022-05-13 12:47:07 +0100

    modify favourites and routes files

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -5,12 +5,28 @@ class FavouritesController < ApplicationController
   end
 
   def create
-    favourite = Favourite.new(user_id: current_user.id, house_id: params[:house_id])
+    favourite = Favourite.new(favourite_params)
 
     if favourite.save
       render json: { favourite: 'we added to your favourites' }, status: 201
     else
       render json: { error: favourite.errors.full_messages }, status: 401
     end
+  end
+
+  def destroy
+    favourite = Favourite.find(params[:id])
+
+    if favourite.destroy
+      head :no_content
+    else
+      render json: { error: favourite.errors.messages }, status: 422
+    end
+   end
+
+  private
+
+  def favourite_params
+    params.require(:favourite).permit(:title, :description, :score, :house_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create]
-  resources :registrations, only: [:create]
+  root 'home#index'
+  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+   post 'auth/signup', to: 'registrations#create'
+   post 'auth/signin', to: 'sessions#create'
+   get 'auth/signout', to: 'sessions#destroy'
+
+  resources :registrations, only: [:new, :create, :destroy]
   resources :houses
   resources :favourites
 end
