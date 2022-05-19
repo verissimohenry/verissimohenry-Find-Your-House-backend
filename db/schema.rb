54956fdede0d46c63c4565a75f9c10f5a61f8c87
verissimohenry <verissimohenry04@gmail.com>
2022-05-19 12:47:17 +0100

    add foreign keys to users and houses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_173805) do
+ActiveRecord::Schema.define(version: 2022_05_19_114609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,4 +40,6 @@ ActiveRecord::Schema.define(version: 2021_12_26_173805) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "favourites", "houses"
+  add_foreign_key "favourites", "users"
 end
