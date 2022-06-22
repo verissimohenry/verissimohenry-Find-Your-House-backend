ced859b22cb821bbdc4e88207668714199c56c16
verissimohenry <verissimohenry04@gmail.com>
2022-06-22 14:12:05 +0100

    Modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,83 @@ House.create([
       photo: "https://2.bp.blogspot.com/-34ca1GHm0mw/VuL-Le43mUI/AAAAAAAAAsU/dqdKDH2cErM6GZqY91PFhPEWWyKg9AWTg/s320/Wooden-House-Design-Minimalist-Classic-and-Simple-Building.jpg"
     }
 ])
+
+House.create([
+    {
+      name: "Home Away From Home",
+      description: 'Classic and Simple - Building a home is about show who the person is by make the house a reflextion of them, one of which build a house with nice material. strong and good quality.',
+      location: "Potuga",
+      price: 75000,
+      photo: "https://cdn.pixabay.com/photo/2016/08/16/03/50/exterior-1597098_960_720.jpg"
+    }
+])
+
+House.create([
+    {
+      name: "Galician castle",
+      description: 'Classic and Simple - Building a home is about show who the person is by make the house a reflextion of them, one of which build a house with nice material. strong and good quality.Wooden House Design Minimalist Classic and Simple - Building a house can be built with any materials, one of which build a house with wood material. wood is strong and good quality.',
+      location: "France",
+      price: 75000,
+      photo: "https://cdn.pixabay.com/photo/2016/09/26/12/02/halic-castle-1695806_960_720.jpg"
+    }
+])
+
+House.create([
+    {
+      name: "Summer Beach House",
+      description: 'Classic and Simple - Building a home is about show who the person is by make the house a reflextion of them, one of which build a house with nice material. strong and good quality.',
+      location: "Miami florida",
+      price: 100000,
+      photo: "https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271_960_720.jpg"
+    }
+])
+
+House.create([
+    {
+      name: "Nice Family Home",
+      description: 'Classic and Simple - Building a home is about show who the person is by make the house a reflextion of them, one of which build a house with nice material. strong and good quality.',
+      location: "U.S.A",
+      price: 35000,
+      photo: "https://cdn.pixabay.com/photo/2017/07/08/02/16/house-2483336_960_720.jpg"
+    }
+])
+
+House.create([
+    {
+      name: "Sky View Apartment",
+      description: 'Classic and Simple - Building a home is about show who the person is by make the house a reflextion of them, one of which build a house with nice material. strong and good quality.',
+      location: "Brazil",
+      price: 150000,
+      photo: "https://cdn.pixabay.com/photo/2018/10/28/12/44/interior-3778708_960_720.jpg"
+    }
+])
+
+House.create([
+    {
+      name: "Luxury Home",
+      description: 'Classic and Simple - Building a home is about show who the person is by make the house a reflextion of them, one of which build a house with nice material. strong and good quality.',
+      location: "L.A",
+      price: 65000,
+      photo: "https://cdn.pixabay.com/photo/2014/05/08/22/30/villa-340451_960_720.jpg"
+    }
+])
+
+House.create([
+    {
+      name: "Villa Home",
+      description: 'Classic and Simple - Building a home is about show who the person is by make the house a reflextion of them, one of which build a house with nice material. strong and good quality.',
+      location: "L.A",
+      price: 150000,
+      photo: "https://cdn.pixabay.com/photo/2019/10/17/02/39/villa-4555824_960_720.jpg"
+    }
+])
+
+House.create([
+    {
+      name: "Pool House",
+      description: 'Classic and Simple - Building a home is about show who the person is by make the house a reflextion of them, one of which build a house with nice material. strong and good quality.',
+      location: "L.A",
+      price: 85000,
+      photo: "https://cdn.pixabay.com/photo/2019/06/13/19/10/pool-house-4272310_960_720.jpg"
+    }
+])
